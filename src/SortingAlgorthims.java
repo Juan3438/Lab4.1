475a856620fc9eef953d4e2e5321017b3c066839
BT_1N3_05@bktech.local <BT_1N3_05@bktech.local>
2018-11-16 10:16:40 -0500

    Initial commit

---
+++ src/SortingAlgorthims.java
@@ -0,0 +1,11 @@
+public class SortingAlgorthims {
+    public static void insertionSort(int[] list1)
+    {
+         int min =0;
+         int pos =0;
+         for(int i = 0; i < arr.length){
+             
+         }
+
+    }
+}
